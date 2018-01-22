fd046ecffe9286713e6ba820adfc3bc41eae7fd6
amtize <tgraes@hotmail.com>
2018-01-22 15:44:48 +0100

    First commit

---
+++ STM32/definitions.h
@@ -0,0 +1,7 @@
+/* definitions.h defines variables and structs used in the program. */
+
+
+struct Point {
+	float x, y, z;
+};
+
---
+++ STM32/workflow.c
@@ -0,0 +1,23 @@
+/* This is a document explaining the workflow and methods used in the project.
+ *
+ *
+ *
+ * Structs:
+ * 	Point {
+ * 		int x,
+ * 		int y,
+ * 		int z
+ * 	}
+ *
+ *
+ * Methods:
+ * 	int move_linear(Point point)
+ * 	int move_bezier(Point point, Point *bezier_points)
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
